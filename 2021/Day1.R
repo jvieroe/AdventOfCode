23d54d0f2c1a69d47770a59d9bc9d0d72d922083
jvieroe <jvieroe@gmail.com>
2021-12-01 19:33:26 +0100

    day1

--- 2021/Day1.R
+++ 2021/Day1.R
@@ -1,47 +1,27 @@
-library(tidyverse)
-library(tidyselect)
-
 # -------------------- DAY 1 --------------------
+library(tidyverse)
 
 # ---------- Part One ----------
-
 input <- read_lines("2021/input_data/d1_input_text.txt") %>% 
   tibble() %>% 
   rename(measurement = '.') %>% 
   mutate(measurement = as.numeric(measurement))
 
-df <- input %>% 
-  mutate(lag_measurement = dplyr::lag(measurement, 1))
-
-df <- df %>% 
-  mutate(increase = ifelse(measurement > lag_measurement,
-                           1,
-                           0))
-
-df %>% 
-  filter(increase == 1) %>% 
+input %>% 
+  mutate(lag_measurement = lag(measurement, 1)) %>% 
+  filter(measurement > lag_measurement) %>% 
   tally()
 
-
-
 # ---------- Part Two ----------
-df <- input %>% 
-  mutate(l1 = dplyr::lag(measurement, 1),
-         l2 = dplyr::lag(measurement, 2)) %>% 
+input %>% 
+  mutate(l1 = lag(measurement, 1),
+         l2 = lag(measurement, 2)) %>% 
   rowwise() %>%
-  mutate(three_window = sum(measurement,
-                            l1,
-                            l2)) %>% 
-  ungroup()
-
-df <- df %>% 
-  mutate(lag_three_window = dplyr::lag(three_window, 1))
-
-df <- df %>% 
-  mutate(increase = ifelse(three_window > lag_three_window,
-                           1,
-                           0))
-
-df %>% 
-  filter(increase == 1) %>% 
+  mutate(tw = sum(c(measurement,
+                    l1,
+                    l2),
+                  na.rm = F)) %>% 
+  ungroup() %>% 
+  mutate(l_tw = lag(tw, 1)) %>% 
+  filter(tw > l_tw) %>% 
   tally()
