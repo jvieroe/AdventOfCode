c86a9c9ab9a4766293f26b6f508f7adffc6e26ad
Jeppe Vierø <43220957+jvieroe@users.noreply.github.com>
2021-12-06 17:56:01 +0100

    Update Day4.R

--- 2021/Day4.R
+++ 2021/Day4.R
@@ -1,5 +1,6 @@
 # -------------------- DAY 4 --------------------
 library(tidyverse)
+library(tidyselect)
 
 numbers <- read_lines("2021/input_data/d4_numbers.txt") %>% 
   str_split(",") %>% 
@@ -15,14 +16,43 @@ boards <- input %>%
   mutate(nn = str_trim(nn)) %>% 
   separate(nn, into = paste0("v",
                              seq(1:5))) %>% 
-  filter(!is.na(v5))
+  filter(!is.na(v5)) %>% 
+  mutate(across(names(.),
+                ~ as.numeric(.x)))
+
+nms <- names(boards)
 
 boards <- boards %>% 
   mutate(board = sort(rep(seq(1, 100), 5))) %>% 
   group_by(board) %>% 
-  mutate(row = row_number()) %>% 
-  ungroup()
+  mutate(row = row_number())
 
+df <- boards
 
+numbers <- numbers[1:10]
 
 # ---------- Part One ----------
+for (i in seq_along(numbers)) {
+  
+  print(i)
+  
+  #print(numbers[i])
+  
+  df <- df %>% 
+    mutate(across(all_of(nms),
+                  ~ ifelse(.x == numbers[i],
+                           0,
+                           .x))) %>% 
+    # mutate(rs = pmap_dbl(select(., nms),
+    #                      function(...) mean(c(...))))
+    # mutate(rs = rowMeans(subset(., select = c(v1, v2, v3, v4, v5)),
+    #                      na.rm = TRUE))
+    # mutate(rs = rowMeans(select(., nms)))
+    rowwise() %>%
+    mutate(is_na = sum(c(nms),
+                       na.rm = F)) %>%
+    ungroup()
+  
+}
+
+
